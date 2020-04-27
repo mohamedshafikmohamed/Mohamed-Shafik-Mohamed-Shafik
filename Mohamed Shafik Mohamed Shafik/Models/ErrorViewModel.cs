fccaf7c5b687ce172bbe128f640db8acff40cebd
mohamedshafikmohamed <48866403+mohamedshafikmohamed@users.noreply.github.com>
2020-04-28 00:41:16 +0200

    Add project files.

---
+++ Mohamed Shafik Mohamed Shafik/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mohamed_Shafik_Mohamed_Shafik.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mohamed Shafik Mohamed Shafik/Models/Parent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mohamed_Shafik_Mohamed_Shafik.Models
+{
+    public class Parent
+    {
+        public Guid Id { get; set; }
+        public string Name  { get; set; }
+        public string image { get; set; }
+    }
+    public class My_info:Parent
+    {
+     
+        public string job  { get; set; }
+    }
+    public class Projects : Parent
+    {
+
+        public string Discription { get; set; }
+    }
+}
